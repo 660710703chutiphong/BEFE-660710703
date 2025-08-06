62a240877effcbef26f5f682f2a13248027695d7
Chutiphong Buranakunkikan <buranakunkikan_c@su.ac.th>
2025-08-06 04:58:59 +0000

    week4-lab

---
+++ week4-lab2/main.go
@@ -0,0 +1,14 @@
+package main
+
+import ("fmt")
+func main(){
+	//var name string = "Chutiphong"
+	var age int = 20
+
+	email := "Buranakunkikan_c@su.ac.th"
+	gpa := 2.77
+
+	firstname,lastname := "Chutiphong" , "Buranakunkikan"
+
+	fmt.Printf("Name: %s %s, age: %d, email: %s, gpa: %.2f \n",firstname,lastname,age,email,gpa)
+}
